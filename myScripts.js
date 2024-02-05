6df37292fdd3b75d3442ec966763753d411caf9b
Christian Zawojski <clzawojski@gmail.com>
2024-02-04 19:07:57 -0500

    copied instruction to README file, and added an example IIFE to myScripts

---
+++ myScripts.js
@@ -0,0 +1,14 @@
+// store the gameboard as an array inside of a Gameboard object
+const gameboard = (function () {
+    const boardArr = [];
+    // example code below
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    return { boardArr };
+    })();
+
+// players are also going to be stored in objects
+
+// an object to control the flow of the game itself.
